af9ea7a5681f77341ab8a55cf39922633d6bf6cf
hamakawataiga <s20020@std.it-college.ac.jp>
2020-06-24 16:23:01 +0900

    ファイルパターンの指定の追加

--- findgrep.sh
+++ findgrep.sh
@@ -2,8 +2,13 @@
 
 pattern=$1
 directory=$2
+name=$3 
+
 if [ -z "$firector" ]; then
 	directory='.'
 fi
+
+if [ -z "$name" ]' then
+	name='*'
 find  "$directory" -type f | xargs -0 grep -nH "$pattern"
 
